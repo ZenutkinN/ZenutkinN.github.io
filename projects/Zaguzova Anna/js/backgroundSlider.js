cac1d4fd284cae12e7752052c2d561c26777eb5e
ZenutkinN <51788586+ZenutkinN@users.noreply.github.com>
2019-12-15 14:24:38 +0700

    Remove project (Anna Zaguzova)

--- projects/Zaguzova Anna/js/backgroundSlider.js
+++
@@ -1,5 +0,0 @@
-let arrPictures = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png'];
-    let backgroundSlider = document.querySelector('.backgroundSlider');
-    let num =  Math.floor(Math.random() * ((arrPictures.length-1) - 0) + 0);
-    backgroundSlider.style.backgroundImage = 'url("img/fon/' + arrPictures[num] + '")';
-        
--- projects/Zaguzova Anna/js/targetOff.js
+++
@@ -1,9 +0,0 @@
-var hashLinks = document.querySelectorAll("a[href^='#']");
-        [].forEach.call(hashLinks, function (link) {
-            link.addEventListener("click", function (event) {
-                event.preventDefault();
-                history.pushState({}, "", link.href);
-                history.pushState({}, "", link.href);
-                history.back();
-            });
-        });
